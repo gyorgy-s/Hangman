81f72379bb208fa339bfc42f922293a044cd989d
gyorgy-s <140075614+gyorgy-s@users.noreply.github.com>
2023-07-21 14:26:14 +0200

    feat: added input feedback of the choice
    feedback for the user, if the guessed letter is or is not in the word, in addition keeping track of previous choices

--- Hangman.py
+++ Hangman.py
@@ -19,34 +19,53 @@ word_hidden = list(random.choice(word_list.list))
 word_shown = list("_" * len(word_hidden))
 
 choice = ""
+choices = []
+
+message = ""
 
 life = 6
 
 clear()
 
 print(ascii_art.logo)
-
 print("{0} ".format("  ".join(word_shown)))
 
 # while the user has lives, keep asking for an input, which should be a alphabetical charater, the match that input to the characters of the chosen word. If the characers match, replace the corresponding blank in "word_hidden" to the character. If there is no match, decrease the number of lives, and print the corresponding ascii art. Then repeat.
+# Additional message, if the chosen letter is or is not in the word, and store and check if the letter was guessed before.
 while life > 0:
     if "_" in word_shown:
+        choice = ""
 
         while len(choice)!=1 or not choice.isalpha():
             choice = input("Guess a letter: ").lower()
             
         if word_hidden.count(choice):       #alternate condition: if choice is not in word_hidden:
-            for i in range(len(word_hidden)):
-                if choice == word_hidden[i]:
-                    word_shown[i] = choice
+            if choice in choices:
+                message = f"you've already tried \"{choice}\", it's in the word."
+            else:
+                choices.append(choice)
+                message = f"it's in the word"
+                for i in range(len(word_hidden)):
+                    if choice == word_hidden[i]:
+                        word_shown[i] = choice
         else :
-            life -= 1  
-
-        choice = ""
+            if choice in choices:
+                message = f"you've already tried \"{choice}\", it's not in the word."
+            else:
+                choices.append(choice)
+                message = f"it's not in the word"
+                life -= 1  
+        
+        
         clear()
+        
         print(ascii_art.logo)
+        # print(word_hidden)
+        
         print("{0} ".format("  ".join(word_shown)))
+        
         print(ascii_art.stages[life])
+        print(f"You've guessed \"{choice}\", " + message)
         print("Life remaining:", life)
     
     else:
